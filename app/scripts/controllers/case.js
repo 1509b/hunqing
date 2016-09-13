10873fbad58067f4c9bddb768d0a710fb4b77e68
成昊达 <1308802232@qq.com>
2016-09-13 08:54:13 +0800

    chd  固定导航、返回顶部

--- app/scripts/controllers/case.js
+++ app/scripts/controllers/case.js
@@ -9,5 +9,10 @@
  */
 angular.module('yoApp')
   .controller('case', function () {
-   
-  });
+	$(".c_wedding_pic img").mouseover(function(){
+		$(this).fadeTo("fast",1).siblings().fadeTo("fast",0.7)
+		$(this).parent().siblings().children().fadeTo("fast",0.7)
+	})
+	
+
+});
