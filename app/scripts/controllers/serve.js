aced1487debc927a60898d8c26dcd5bad339ad95
李孟圆 <1198203538@qq.com>
2016-09-13 11:27:23 +0800

    嗯，是的，又改了改

--- app/scripts/controllers/serve.js
+++ app/scripts/controllers/serve.js
@@ -9,5 +9,24 @@
  */
 angular.module('yoApp')
   .controller('serve', function () {
+    $(function(){
+        $('#btn').onclick=function(){
+            if(mv.paused){
+                mv.play();
+            }else{
+                mv.pause();
+            }
+        }
+        $(".l-content2").hover(function(){
+            $(".l-content2 span").addClass('hover');
+        },function(){
+            $(".l-content2 span").removeClass('hover');
+        })
+        $(".l-content4-right").hover(function(){
+            $(".l-content4-right span").addClass('hover');
+        },function(){
+            $(".l-content4-right span").removeClass('hover');
+        })
+    })
 
-  });
+    });
