df959102029ecbd43a4538122193fe2b1cd8aaf2
马晨亮 <571950737@qq.com>
2016-09-22 14:12:23 +0800

    autoplay

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -34,7 +34,6 @@ angular.module('yoApp')
    			})
    		})
    		
-<<<<<<< HEAD
 		$http({
 			url:"http://123.56.227.177:2504/wedding-autoplay",
 			method:"get",
@@ -45,73 +44,27 @@ angular.module('yoApp')
 		})	
 			//http://123.56.227.177/bi2/public/wedding_upload/images/{{i.img}}
 
-  $scope.myInterval = 5000;
-  $scope.noWrapSlides = false;
-  $scope.active = 0;
-  var slides = $scope.slides = [];
-  var currIndex = 0;
 
-  $scope.addSlide = function() {
-    var newWidth = 600 + slides.length + 1;
-    slides.push({
-      image: '//unsplash.it/' + newWidth + '/300',
-      text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
-      id: currIndex++
-    });
-  };
-
-  $scope.randomize = function() {
-    var indexes = generateIndexesArray();
-    assignNewIndexesToSlides(indexes);
-  };
-
-  for (var i = 0; i < 4; i++) {
-    $scope.addSlide();
-  }
-
-  // Randomize logic below
-
-  function assignNewIndexesToSlides(indexes) {
-    for (var i = 0, l = slides.length; i < l; i++) {
-      slides[i].id = indexes.pop();
-    }
-  }
-
-  function generateIndexesArray() {
-    var indexes = [];
-    for (var i = 0; i < currIndex; ++i) {
-      indexes[i] = i;
-    }
-    return shuffle(indexes);
-  }
-
-  // http://stackoverflow.com/questions/962802#962890
-  function shuffle(array) {
-    var tmp, current, top = array.length;
-
-    if (top) {
-      while (--top) {
-        current = Math.floor(Math.random() * (top + 1));
-        tmp = array[current];
-        array[current] = array[top];
-        array[top] = tmp;
-      }
-    }
-
-    return array;
-  }
-
-	
-
-=======
->>>>>>> origin/master
-   		
-  });
-
-// <slide ng-repeat="slide in slides" active="slide.active">
-//      	{{i.img}}
-//          <img ng-src="{{slide.image}}">
-//          <div class="carousel-caption">
-//              <p>{{slide.text}}</p>
-//          </div>
-//      </slide>
+		$scope.myInterval = 2000;
+		$scope.noWrapSlides = false;
+		var slides = $scope.slides = [];
+		$scope.addSlide = function() {
+			var newWidth = 600 + slides.length + 1;
+			slides.push({
+				img: 'img/11.jpg',
+				//text: 'Chocola & Vanilla'
+			});
+			slides.push({
+				img: 'img/22.jpg',
+				//text: 'Chocola & Vanilla'
+			});
+			slides.push({
+				img: 'img/33.jpg',
+				//text: 'Chocola & Vanilla'
+			});
+			slides.push({
+				img: 'img/44.jpg',
+				//text: 'Chocola & Vanilla'
+			});
+		};
+		$scope.addSlide();
