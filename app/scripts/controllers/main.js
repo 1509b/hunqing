8e2bbfbd0efdaa3c1ac2d0532e24d9f0ece2e99b
成昊达 <1308802232@qq.com>
2016-09-22 19:05:30 +0800

    angular_选项卡完成

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,13 +8,39 @@
  * Controller of the yoApp
  */
 angular.module('yoApp')
-  .controller('main', function ($scope,$http) {
+  .controller('main', function ($scope,$http,$stateParams) {
 		$scope.swi=function(e){
 			$scope.tab=e;
 		}
-		$scope.addback=function(e){
-			$scope.i=e
+		$scope.isshow=true
+		$scope.aa=true
+		$scope.a=function(){
+			$scope.isshow=true
+			$scope.isshow1=false
+			$scope.isshow2=false
+			$scope.aa=true
+			$scope.bb=false
+			$scope.cc=false
+			
+		}		
+		$scope.b=function(){
+			$scope.isshow=false
+			$scope.isshow1=true
+			$scope.isshow2=false
+			$scope.aa=false
+			$scope.bb=true
+			$scope.cc=false
+		}		
+		$scope.c=function(){
+			$scope.isshow=false
+			$scope.isshow1=false
+			$scope.isshow2=true	
+			$scope.aa=false
+			$scope.bb=false
+			$scope.cc=true
 		}
+		
+		
 		
 		
 		$http({
@@ -26,33 +52,38 @@ angular.module('yoApp')
 			console.log(e)
 		})
 		
-		$scope.details=function(e){
-			alert(e.id)
-		}
+		
+		$http({
+			url:"http://123.56.227.177:2504/wedding-tab",
+			method:"get",
+			params:{classify:0},
+			withCredentials:true 
+		}).success(function(x){
+			$scope.data1=x
+			console.log(x)
+		})
+		
+		$http({
+			url:"http://123.56.227.177:2504/wedding-tab",
+			method:"get",
+			params:{classify:1},
+			withCredentials:true 
+		}).success(function(y){
+			$scope.data2=y
+			console.log(y)
+		})
+		
+		$http({
+			url:"http://123.56.227.177:2504/wedding-tab",
+			method:"get",
+			params:{classify:2},
+			withCredentials:true 
+		}).success(function(z){
+			$scope.data3=z
+			console.log(z)
+		})
 		
 		
-
-
-   		$(function(){
-   			$(".song-div5-con-1").click(function(){
-   				$(".song-div5-con-1").css("background","#ff6637").siblings().css("background","white")
-   				$(".song-div5-con-1-p1").css("color","white")
-   				$(".song-div5-con-2-p2").css("color","#999797")
-   				$(".song-div5-con-3-p3").css("color","#999797")
-   			})
-   			$(".song-div5-con-2").click(function(){
-   				$(".song-div5-con-2").css("background","#ff6637").siblings().css("background","white")
-   				$(".song-div5-con-2-p2").css("color","white")
-   					$(".song-div5-con-1-p1").css("color","#999797")
-   				$(".song-div5-con-3-p3").css("color","#999797")
-   			})
-   			$(".song-div5-con-3").click(function(){
-   				$(".song-div5-con-3").css("background","#ff6637").siblings().css("background","white")
-   				$(".song-div5-con-3-p3").css("color","white")
-   					$(".song-div5-con-1-p1").css("color","#999797")
-   				$(".song-div5-con-2-p2").css("color","#999797")
-   			})
-   		})
    		
 		$http({
 			url:"http://123.56.227.177:2504/wedding-autoplay",
@@ -126,11 +157,12 @@ angular.module('yoApp')
    		
 
   }).filter("hq",function(){
-  	return function(q){
-  		if(q.length>280){
-  			return q.substr(0,280)+'...'
+  	return function(e){
+//		alert(typeof(q))
+  		if(e.length>280){
+  			return e.substr(0,280)+'...'
   		}else{
-  			return q
+  			return e
   		}
   	}
   })
