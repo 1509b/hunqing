a677e183ed856a399abaed17bd746e01ff0e952c
李孟圆 <1198203538@qq.com>
2016-09-22 19:10:36 +0800

    top

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -9,18 +9,11 @@
  */
 angular.module('yoApp')
     .controller('about', function ($scope,$http) {
-        //$http({
-        //    url:"http://",
-        //    method:"GET",
-        //    withCredentials:true
-        //}).success(function(e){
-        //
-        //})
         $scope.mouseenter = function () {
             $('.l-about-content2 span').addClass('hover');
          }
         $scope.mouseleave = function () {
-            $('.l-about-content2 span').removeClass('hover');
+            $('.l-about-content62 span').removeClass('hover');
         }
         $scope.mouseenter1 = function () {
             $('.span1 a').addClass('hover');
@@ -36,14 +29,18 @@ angular.module('yoApp')
         }
         //验证信息
         var telphone=/^1[34578]\d{9}$/;
+        var  reg=0;
+        var reg1=0
         $scope.telclick=function(){
             $(".tel+b").html("请正确输入手机号码");
         }
         $scope.telblur=function(){
             var tel=$(".tel").val();
             if(telphone.test(tel) && tel!=""){
+                reg=0;
               $(".tel+b").html("输入正确");
             }else{
+                reg=1;
               $(".tel+b").html("请核对信息");
             }
         }
@@ -54,19 +51,35 @@ angular.module('yoApp')
         $scope.mailblur=function(){
             var mail=$(".mail").val();
             if(mailreg.test(mail) && mail!=""){
+                reg1=0;
                 $(".mail+b").html("输入正确");
             }else{
+                reg1=1
                 $(".mail+b").html("请核对信息");
             }
         }
-        //$scope.sub=function(){
-        //    $http({
-        //        url:'http://localhost:2403/upload',
-        //        method:"POST",
-        //    }).success(function(e){
-        //        console.log(e)
-        //    })
-        //}
+        $scope.sub=function(e){
+            if(reg==0&&reg1==0){
+                //submit();
+                $http({
+                    url:"http://123.56.227.177:2504/wedding-commit",
+                    method:"post",
+                    data:$scope.updata,
+                    withCredentials:true
+                }).success(function(e){
+                    $scope.data.push(e);
+                })
+            }else{
+                alert("您输入的信息有误")
+            }
+        }
+
+    /*WEDDING-COMMIT
+     name
+     tel
+     email
+     request
+     */
 
     });
 
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,60 +8,52 @@
  * Controller of the yoApp
  */
 angular.module('yoApp')
-<<<<<<< HEAD
-  .controller('main', function($scope,$http) {
-=======
-  .controller('main', function ($scope,$http) {
+	.controller('main', function ($scope,$http) {
 		$scope.swi=function(e){
 			$scope.tab=e;
 		}
 		$scope.addback=function(e){
 			$scope.i=e
 		}
-		
-		
+
+
 		$http({
 			url:"http://123.56.227.177:2504/wedding-news",
 			method:"get",
-			withCredentials:true  //withCredentials:true æŠŠåŽå°è¿”çš„cookieå†ä¼ ç»™åŽå°  
+			withCredentials:true  //withCredentials:true °ÑºóÌ¨·µµÄcookieÔÙ´«¸øºóÌ¨  
 		}).success(function(e){
 			$scope.data=e
 			console.log(e)
 		})
-		
+
 		$scope.details=function(e){
 			alert(e.id)
 		}
-		
-		
 
 
->>>>>>> origin/master
-   		$(function(){
-   			$(".song-div5-con-1").click(function(){
-   				$(".song-div5-con-1").css("background","#ff6637").siblings().css("background","white")
-   				$(".song-div5-con-1-p1").css("color","white")
-   				$(".song-div5-con-2-p2").css("color","#999797")
-   				$(".song-div5-con-3-p3").css("color","#999797")
-   			})
-   			$(".song-div5-con-2").click(function(){
-   				$(".song-div5-con-2").css("background","#ff6637").siblings().css("background","white")
-   				$(".song-div5-con-2-p2").css("color","white")
+
+
+		$(function(){
+			$(".song-div5-con-1").click(function(){
+				$(".song-div5-con-1").css("background","#ff6637").siblings().css("background","white")
+				$(".song-div5-con-1-p1").css("color","white")
+				$(".song-div5-con-2-p2").css("color","#999797")
+				$(".song-div5-con-3-p3").css("color","#999797")
+			})
+			$(".song-div5-con-2").click(function(){
+				$(".song-div5-con-2").css("background","#ff6637").siblings().css("background","white")
+				$(".song-div5-con-2-p2").css("color","white")
 				$(".song-div5-con-1-p1").css("color","#999797")
-   				$(".song-div5-con-3-p3").css("color","#999797")
-   			})
-   			$(".song-div5-con-3").click(function(){
-   				$(".song-div5-con-3").css("background","#ff6637").siblings().css("background","white")
-   				$(".song-div5-con-3-p3").css("color","white")
+				$(".song-div5-con-3-p3").css("color","#999797")
+			})
+			$(".song-div5-con-3").click(function(){
+				$(".song-div5-con-3").css("background","#ff6637").siblings().css("background","white")
+				$(".song-div5-con-3-p3").css("color","white")
 				$(".song-div5-con-1-p1").css("color","#999797")
-   				$(".song-div5-con-2-p2").css("color","#999797")
-   			})
-   		})
-<<<<<<< HEAD
-		//
-  });
-=======
-   		
+				$(".song-div5-con-2-p2").css("color","#999797")
+			})
+		})
+
 		$http({
 			url:"http://123.56.227.177:2504/wedding-autoplay",
 			method:"get",
@@ -69,79 +61,79 @@ angular.module('yoApp')
 		}).success(function(e){
 			console.log()
 			$scope.data=e
-		})	
-			//http://123.56.227.177/bi2/public/wedding_upload/images/{{i.img}}
+		})
+		//http://123.56.227.177/bi2/public/wedding_upload/images/{{i.img}}
 
-  $scope.myInterval = 5000;
-  $scope.noWrapSlides = false;
-  $scope.active = 0;
-  var slides = $scope.slides = [];
-  var currIndex = 0;
+		$scope.myInterval = 5000;
+		$scope.noWrapSlides = false;
+		$scope.active = 0;
+		var slides = $scope.slides = [];
+		var currIndex = 0;
 
-  $scope.addSlide = function() {
-    var newWidth = 600 + slides.length + 1;
-    slides.push({
-      image: '//unsplash.it/' + newWidth + '/300',
-      text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
-      id: currIndex++
-    });
-  };
+		$scope.addSlide = function() {
+			var newWidth = 600 + slides.length + 1;
+			slides.push({
+				image: '//unsplash.it/' + newWidth + '/300',
+				text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
+				id: currIndex++
+			});
+		};
 
-  $scope.randomize = function() {
-    var indexes = generateIndexesArray();
-    assignNewIndexesToSlides(indexes);
-  };
+		$scope.randomize = function() {
+			var indexes = generateIndexesArray();
+			assignNewIndexesToSlides(indexes);
+		};
 
-  for (var i = 0; i < 4; i++) {
-    $scope.addSlide();
-  }
+		for (var i = 0; i < 4; i++) {
+			$scope.addSlide();
+		}
 
-  // Randomize logic below
+		// Randomize logic below
 
-  function assignNewIndexesToSlides(indexes) {
-    for (var i = 0, l = slides.length; i < l; i++) {
-      slides[i].id = indexes.pop();
-    }
-  }
+		function assignNewIndexesToSlides(indexes) {
+			for (var i = 0, l = slides.length; i < l; i++) {
+				slides[i].id = indexes.pop();
+			}
+		}
 
-  function generateIndexesArray() {
-    var indexes = [];
-    for (var i = 0; i < currIndex; ++i) {
-      indexes[i] = i;
-    }
-    return shuffle(indexes);
-  }
+		function generateIndexesArray() {
+			var indexes = [];
+			for (var i = 0; i < currIndex; ++i) {
+				indexes[i] = i;
+			}
+			return shuffle(indexes);
+		}
 
-  // http://stackoverflow.com/questions/962802#962890
-  function shuffle(array) {
-    var tmp, current, top = array.length;
+		// http://stackoverflow.com/questions/962802#962890
+		function shuffle(array) {
+			var tmp, current, top = array.length;
 
-    if (top) {
-      while (--top) {
-        current = Math.floor(Math.random() * (top + 1));
-        tmp = array[current];
-        array[current] = array[top];
-        array[top] = tmp;
-      }
-    }
+			if (top) {
+				while (--top) {
+					current = Math.floor(Math.random() * (top + 1));
+					tmp = array[current];
+					array[current] = array[top];
+					array[top] = tmp;
+				}
+			}
 
-    return array;
-  }
-
-	
+			return array;
+		}
 
 
-   		
 
-  }).filter("hq",function(){
-  	return function(q){
-  		if(q.length>280){
-  			return q.substr(0,280)+'...'
-  		}else{
-  			return q
-  		}
-  	}
-  })
+
+
+
+	}).filter("hq",function(){
+		return function(q){
+			if(q.length>280){
+				return q.substr(0,280)+'...'
+			}else{
+				return q
+			}
+		}
+	})
 
 // <slide ng-repeat="slide in slides" active="slide.active">
 //      	{{i.img}}
@@ -150,4 +142,3 @@ angular.module('yoApp')
 //              <p>{{slide.text}}</p>
 //          </div>
 //      </slide>
->>>>>>> origin/master
